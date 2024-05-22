535d82069f37337989e61466d2a96dadfa4923db
Jeff McClintock <jef@synthedit.com>
2024-05-23 09:54:55 +1200

    feat(cm) : Added IDE Print Value module for diagnostics

---
+++ JeffMcClintock/IDE Print Value/IDEPrintValue.cpp
@@ -0,0 +1,65 @@
+/* Copyright (c) 2007-2023 SynthEdit Ltd
+
+Permission to use, copy, modify, and /or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS.IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+#include "mp_sdk_audio.h"
+
+using namespace gmpi;
+
+SE_DECLARE_INIT_STATIC_FILE(IDEPrintValue);
+
+class IDEPrintValue final : public MpBase2
+{
+	AudioInPin pinInput1;
+	StringInPin pinName;
+	float lastValue = 123456789.0f;
+
+public:
+	IDEPrintValue()
+	{
+		initializePin(pinInput1);
+		initializePin(pinName);
+	}
+
+	void subProcess( int sampleFrames )
+	{
+		// get pointers to in/output buffers.
+		auto input1 = getBuffer(pinInput1);
+
+		for( int s = sampleFrames; s > 0; --s )
+		{
+			// TODO: Signal processing goes here.
+
+			// Increment buffer pointers.
+			++input1;
+		}
+	}
+
+	void onSetPins() override
+	{
+		if (lastValue != pinInput1.getValue())
+		{
+			lastValue = pinInput1.getValue();
+			// Output to console.
+			_RPTWN(0, L"%s streaming[%d] val %f\n", pinName.getValue().c_str(), (int)pinInput1.isStreaming(), pinInput1.getValue());
+		}
+
+		// Set processing method.
+		setSubProcess(&IDEPrintValue::subProcess);
+	}
+};
+
+namespace
+{
+	auto r = Register<IDEPrintValue>::withId(L"SE IDE Print Value");
+}
